a70ddaccb30dc27151ab54dd86bf85941623f708
m4ninja <arsharma9966@gmail.com>
2026-05-17 15:08:38 +0530

    Add Jump Game III solution

---
+++ 1306_jump_game_iii.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def canReach(self, arr: list[int], start: int) -> bool:
+
+        n = len(arr)
+
+        queue = deque([start])
+        visited = set([start])
+
+        while queue:
+
+            i = queue.popleft()
+
+            if arr[i] == 0:
+                return True
+
+            for nxt in (i + arr[i], i - arr[i]):
+
+                if 0 <= nxt < n and nxt not in visited:
+                    visited.add(nxt)
+                    queue.append(nxt)
+
+        return False
