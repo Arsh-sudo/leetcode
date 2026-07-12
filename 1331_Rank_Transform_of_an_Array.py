96b11079fbf812380b8d4efde3ede8921e70db48
m4ninja <arsharma9966@gmail.com>
2026-07-13 01:19:56 +0530

    Add Rank Transform of an Array Solution

---
+++ 1331_Rank_Transform_of_an_Array.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def arrayRankTransform(self, arr):
+        rank = {}
+        for i, num in enumerate(sorted(set(arr)), 1):
+            rank[num] = i
+        return [rank[num] for num in arr]
