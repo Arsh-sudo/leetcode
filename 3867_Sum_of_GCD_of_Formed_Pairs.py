9701a4f3f676b4a7cb30d11a452a269cc257f59b
m4ninja <arsharma9966@gmail.com>
2026-07-16 18:51:06 +0530

    Add Sum of GCD of Formed Pairs Solution

---
+++ 3867_Sum_of_GCD_of_Formed_Pairs.py
@@ -0,0 +1,22 @@
+from math import gcd
+
+class Solution:
+    def gcdSum(self, nums):
+        arr = []
+        mx = 0
+
+        for x in nums:
+            mx = max(mx, x)
+            arr.append(gcd(x, mx))
+
+        arr.sort()
+
+        ans = 0
+        i, j = 0, len(arr) - 1
+
+        while i < j:
+            ans += gcd(arr[i], arr[j])
+            i += 1
+            j -= 1
+
+        return ans
